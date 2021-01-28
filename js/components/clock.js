c8adb77489d38006df112d4730cf352ebc8d14ee
oswiux <35800600+oswiux@users.noreply.github.com>
2021-01-28 16:48:58 +0200

    clock updatintas

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,6 +1,26 @@
-function clock(selector) {
+function clock(selector, deadline) {
+    // input validation
+    if (typeof selector !== 'string' ||
+        selector === '') {
+        console.error('ERROR: netinkamo formato selectorius.');
+        return false;
+    }
+    if (typeof deadline !== 'string' ||
+        deadline === '' ||
+        !isFinite((new Date(`2000-${deadline}`)).getTime())) {
+        console.error('ERROR: netinkamo formato deadline reiksme.');
+        return false;
+    }
+
+    // logic
     const clockDOM = document.querySelector(selector);
-    const numbers = [432, 9, 37, 39];
+    if (!clockDOM) {
+        console.error('ERROR: pagal pateikta selectoriu nepavyko rasti norimo DOM elemento.');
+        return false;
+    }
+
+    let allValuesDOM = null;
+    let numbers = calcTime(deadline);
     const labels = ['days', 'hours', 'minutes', 'seconds'];
     let HTML = '';
 
@@ -12,6 +32,14 @@ function clock(selector) {
     }
 
     clockDOM.innerHTML = HTML;
+    allValuesDOM = document.querySelectorAll(`${selector} .value`);
+
+    setInterval(function () {
+        numbers = calcTime(deadline);
+        for (let i = 0; i < 4; i++) {
+            allValuesDOM[i].innerText = numberFormat(numbers[i]);
+        }
+    }, 1000);
 }
 
 function numberFormat(number) {
@@ -19,6 +47,34 @@ function numberFormat(number) {
         return '0' + number;
     }
     return number;
+}
+
+function calcTime(deadline) {
+    const date = new Date();
+    const now = Date.now();
+    let year = date.getFullYear();
+    let fullDeadline = `${year}-${deadline}`;
+    let fullDeadlineInMiliseconds = (new Date(fullDeadline)).getTime();
+
+    if (fullDeadlineInMiliseconds < now) {
+        year++;
+        fullDeadline = `${year}-${deadline}`;
+        fullDeadlineInMiliseconds = (new Date(fullDeadline)).getTime();
+    }
+
+    const diff = fullDeadlineInMiliseconds - now;
+    const seconds = Math.round(diff / 1000);
+
+    const days = Math.floor(seconds / 60 / 60 / 24);
+    let unusedSeconds = seconds - days * 60 * 60 * 24;
+
+    const hours = Math.floor(unusedSeconds / 60 / 60);
+    unusedSeconds -= hours * 60 * 60;
+
+    const minutes = Math.floor(unusedSeconds / 60);
+    unusedSeconds -= minutes * 60;
+
+    return [days, hours, minutes, unusedSeconds];
 }
 
 export { clock }
